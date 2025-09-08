bf60e84765c75bf6fb10f62228095f3e8141c74c
khs2973 <h1372@naver.com>
2025-09-08 21:43:25 +0900

    project init

---
+++ src/main/java/com/cafe/cafetdd/spring/CafeTddApplication.java
@@ -0,0 +1,13 @@
+package com.cafe.cafetdd.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeTddApplication.class, args);
+	}
+
+}
